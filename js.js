681ab0c91c6bda94d95c6f9b816a13287d79f931
Mick Jasinski <m.jasinski@minimalcraft.net>
2017-12-06 01:15:40 +0000

    Fix misspelling

--- js.js
+++ js.js
@@ -1,9 +1,9 @@
-var text = 'Velociraptor is a genus of herbivorous ceratopsid dinosaur that first appeared during the late Maast chtian stage of the late Cretaceous period.';
+var text = 'Velociraptor is a genus of herbivorous ceratopsid dinosaur that first appeared during the late Maastrichtian stage of the late Cretaceous period.';
 var dinosaur ='Triceratops';
 
 var dinoUprCase = dinosaur.toUpperCase();
 
 var textFix = text.replace('Velociraptor', dinosaur);
 
-var textClip = textFix.slice(0, textFix.length/2);
+var textClip = textFix.slice(0, textFix.length / 2);
 console.log(textClip);
